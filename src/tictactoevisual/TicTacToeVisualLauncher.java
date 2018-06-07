3874c10a8c504bcead8cc04f80e1aa6997f3f287
Joshua Kopen <jmk86@duke.edu>
2018-06-06 17:29:08 -0700

    Finished the basic visual implementation, code still needs to be cleaned up

--- src/tictactoevisual/TicTacToeVisualLauncher.java
+++ src/tictactoevisual/TicTacToeVisualLauncher.java
@@ -1,17 +1,40 @@
 package tictactoevisual;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 import tictactoe.TicTacToeRunner;;
 
 public class TicTacToeVisualLauncher extends Application {
+	
+    private static final int FRAMES_PER_SECOND = 60;
+    private static final int MILLISECOND_DELAY = 1000 / FRAMES_PER_SECOND;
+    private static final double SECOND_DELAY = 1.0 / FRAMES_PER_SECOND;
+    
+    private TicTacToeVisualRunner tttr;
+    private Stage s;
 
 	public static void main(String[] args) {
 		launch(args);
 	}
 	
 	public void start(Stage s) {
-		TicTacToeVisualRunner tttr = new TicTacToeVisualRunner();
-		tttr.runGameTwoPlayer(s);
+		this.s = s;
+		tttr = new TicTacToeVisualRunner();
+		s.setScene(tttr.setupBoard());
+		
+		s.show();
+        // attach "game loop" to timeline to play it
+        KeyFrame frame = new KeyFrame(Duration.millis(MILLISECOND_DELAY),
+                                      e -> step(SECOND_DELAY));
+        Timeline animation = new Timeline();
+        animation.setCycleCount(Timeline.INDEFINITE);
+        animation.getKeyFrames().add(frame);
+        animation.play();
 	}
 
+	public void step(double elapsedTime) {
+		tttr.runGameTwoPlayer(s);
+	}
 }

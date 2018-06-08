4a25186f101107d4cc34693389b40eebe81e6823
Joshua Kopen <jmk86@duke.edu>
2018-06-07 20:06:25 -0700

    Did more work on the pieces, made a model for a move generator

---
+++ src/chesspieces/MoveGenerator.java
@@ -0,0 +1,12 @@
+package chesspieces;
+
+import java.util.List;
+
+import boardbasics.Board;
+import boardbasics.Coord;
+import boardbasics.Square;
+
+public interface MoveGenerator {
+
+	public List<Square> getMoves(Board board, Coord pos);
+}
---
+++ src/chesspieces/StraightLineMoveGenerator.java
@@ -0,0 +1,19 @@
+package chesspieces;
+
+import java.util.List;
+
+import boardbasics.Board;
+import boardbasics.Coord;
+import boardbasics.Square;
+
+public class StraightLineMoveGenerator implements MoveGenerator{
+
+		
+	@Override
+	public List<Square> getMoves(Board board, Coord pos) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
+
+	

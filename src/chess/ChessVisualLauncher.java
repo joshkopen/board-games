03e32bacdfb004eb76f0b53dc172635837dadf46
Joshua Kopen <jmk86@duke.edu>
2018-06-09 13:22:53 -0700

    Committing to merge master

---
+++ src/chess/ChessVisualLauncher.java
@@ -0,0 +1,16 @@
+package chess;
+
+import javafx.application.Application;
+
+public class ChessVisualLauncher extends Application {
+
+	private static final int FRAMES_PER_SECOND = 60;
+    private static final int MILLISECOND_DELAY = 1000 / FRAMES_PER_SECOND;
+    private static final double SECOND_DELAY = 1.0 / FRAMES_PER_SECOND;
+    
+    public static void main(String[] args) {
+		launch(args);
+	}
+    
+    
+}
